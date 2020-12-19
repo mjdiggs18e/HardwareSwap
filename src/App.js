d57a3cd51a948cc5a5504055d9c4b2ad03128a06
Michael Diggs <mjdiggs18.e@gmail.com>
2020-12-19 13:47:09 -0500

    Fixed page breaking on refresh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import PostingBuy from './Components/PostingBuy';
 import Posts from './Components/Posts';
@@ -15,36 +15,36 @@ import PostList from './Components/PostList';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <UserProvider>
         <Header />
-        <div className='wrapper'>
+        <div className="wrapper">
           <Switch>
-            <Route path='/' exact>
-              <div className='flex-homepage'>
+            <Route path="/" exact>
+              <div className="flex-homepage">
                 <PostList />
                 <PostingBuy />
               </div>
             </Route>
-            <Route path='/sell'>
-              <div className='flex-homepage'>
+            <Route path="/sell">
+              <div className="flex-homepage">
                 <PostList />
                 <PostingSell />
               </div>
             </Route>
-            <Route path='/trade'>
-              <div className='flex-homepage'>
+            <Route path="/trade">
+              <div className="flex-homepage">
                 <PostList />
                 <PostingTrade />
               </div>
             </Route>
-            <Route path='/forgot-password' component={ForgotPassword} />
-            <PrivateRoute path='/signup' component={SignUp} />
-            <PrivateRoute path='/login' component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+            <PrivateRoute path="/signup" component={SignUp} />
+            <PrivateRoute path="/login" component={Login} />
           </Switch>
         </div>
       </UserProvider>
-    </Router>
+    </HashRouter>
   );
 }
 
