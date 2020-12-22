5928508c7ec70b4a0b9eb58fbe2a66bdec641501
Michael Diggs <mjdiggs18.e@gmail.com>
2020-12-22 00:51:10 -0500

    Styled comments

--- src/Components/CommentList.js
+++ src/Components/CommentList.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
 import { useParams } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { useAuth } from '../Context/UserContext';
+import { db } from '../Firebase/firebase';
 
 const CommentList = () => {
   const { id } = useParams();
@@ -25,10 +29,21 @@ const CommentList = () => {
     };
   }, []);
 
+  const { currentUser } = useAuth();
+
   return post.map((comment, index) => {
+    dayjs.extend(relativeTime);
+    const postCreatedAt = dayjs.unix(comment.createdAt);
+    const currentTime = dayjs();
+    const timeSincePost = postCreatedAt.from(currentTime);
+
     return (
-      <div key={index}>
-        <h1>{comment.message}</h1>
+      <div className='comment-post' key={index}>
+        <div className='comment-top'>
+          <p className='comment-author'>Submitted by {comment.sender}</p>
+          <p className='comment-timesince'>{timeSincePost}</p>
+        </div>
+        <p className='comment-message'>{comment.message}</p>
       </div>
     );
   });
